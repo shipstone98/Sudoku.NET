7221ea58a068f248444eb08f59b15520600715fc
Christopher Shipstone <christopher.shipstone98@gmail.com>
2021-02-25 19:47:56 +0000

    Added constructor tests

--- SudokuTest/SudokuTest.cs
+++ SudokuTest/SudokuTest.cs
@@ -1,4 +1,7 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Sudoku;
 
 namespace SudokuTest
 {
@@ -6,8 +9,32 @@ namespace SudokuTest
     public class SudokuTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestConstructor_InvalidSizes()
         {
+            Assert.ThrowsException<ArgumentException>(() => new SudokuPuzzle(2));
+            Assert.ThrowsException<ArgumentException>(() => new SudokuPuzzle(SudokuPuzzle.MaxSize - 1));
         }
+
+        [TestMethod]
+        public void TestConstructor_MaximumSize() => Assert.ThrowsException<ArgumentOutOfRangeException>(() => new SudokuPuzzle(Int32.MaxValue));
+
+        [TestMethod]
+        public void TestConstructor_MaximumPossibleSize() => Assert.ThrowsException<ArgumentOutOfRangeException>(() => new SudokuPuzzle(SudokuPuzzle.MaxSize + 1));
+
+        [TestMethod]
+        public void TestConstructor_MinimumSize() => Assert.ThrowsException<ArgumentOutOfRangeException>(() => new SudokuPuzzle(Int32.MinValue));
+
+        [TestMethod]
+        public void TestConstructor_NegativeOneSize() => Assert.ThrowsException<ArgumentOutOfRangeException>(() => new SudokuPuzzle(-1));
+
+        [TestMethod]
+        public void TestConstructor_ValidSizes()
+        {
+            new SudokuPuzzle(1);
+            new SudokuPuzzle(SudokuPuzzle.MaxSize);
+        }
+
+        [TestMethod]
+        public void TestConstructor_ZeroSize() => Assert.ThrowsException<ArgumentOutOfRangeException>(() => new SudokuPuzzle(0));
     }
 }
